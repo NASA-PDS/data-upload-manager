52f1b4a6d76e6790a7c91577176bf340801a01d4
Scott Collins <collinss@jpl.nasa.gov>
2023-03-17 11:03:18 -0700

    fixup! Added node_util.py module for managing set of valid PDS node identifiers

--- src/pds/ingress/util/node_util.py
+++ src/pds/ingress/util/node_util.py
@@ -10,6 +10,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class NodeUtil:
     """Provides methods to validate PDS Node identifiers."""
 
@@ -22,9 +23,10 @@ class NodeUtil:
         "ppi": "Planetary Plasma Interactions",
         "rs": "Radio Science",
         "rms": "Ring-Moon Systems",
-        "sbn": "Small Bodies"
+        "sbn": "Small Bodies",
     }
 
     @classmethod
     def permissible_node_ids(cls):
+        """Returns a list of the Node IDs accepted by the Ingress client"""
         return cls.node_id_to_long_name.keys()
