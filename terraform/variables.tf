d60448c6bbfc46f696eb5919e306226d04456322
Scott Collins <collinss@jpl.nasa.gov>
2023-09-07 14:26:15 -0700

    Added descriptions for top-level variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,35 +2,43 @@
 variable "region" {
   type        = string
   default     = "us-west-2"
+  description = "AWS region to allocate AWS resources to"
 }
 
 variable "profile" {
-  type = string
+  type        = string
+  description = "Name of the AWS account profile"
 }
 
 variable "credential_file" {
-  type = string
+  type        = string
+  description = "Path to an AWS credentials file on the local machine"
 }
 
 variable "lambda_s3_bucket_name" {
-  type = string
+  type        = string
+  description = "Name of the S3 bucket to upload Lambda artifacts to"
 }
 
 variable "api_gateway_lambda_role_arn" {
-  type = string
+  type        = string
+  description = "ARN for an IAM role which has permissions for both API Gateway and Lambda"
 }
 
 # TODO: this may go away when API is deployed as REGIONAL instead of PRIVATE
 variable "api_gateway_policy_source_vpc" {
-  type = string
+  type        = string
+  description = "ID of the VPC which is allowed to talk to the Private API Gateway deployment"
 }
 
 variable "lambda_ingress_service_iam_role_arn" {
-  type = string
+  type        = string
+  description = "IAM role ARN to allocate to the Ingress Service Lambda function"
 }
 
 variable "lambda_authorizer_iam_role_arn" {
   type = string
+  description = "IAM role ARN to allocate to the Lambda Authorizer function"
 }
 
 variable "nucleus_dum_cognito_initial_users" {
@@ -44,9 +52,11 @@ variable "nucleus_dum_cognito_initial_users" {
       }
     )
   )
+  description = "List of Cognito users to pre-populate the User Pool with"
 }
 
 variable "nucleus_dum_client_cloudwatch_log_group" {
-  type    = string
-  default = "/pds/nucleus/dum/client-log-group"
+  type        = string
+  default     = "/pds/nucleus/dum/client-log-group"
+  description = "Name of the CloudWatch Log Group for use with the DUM Ingress Client script"
 }
