7097cdb7353113e785f15b2eb7dbe03d86c37856
Scott Collins <collinss@jpl.nasa.gov>
2024-08-08 10:25:57 -0700

    Reworked "giveup" function used with backoff decorators to look for an explicit set of HTTP error codes

--- src/pds/ingress/client/pds_ingress_client.py
+++ src/pds/ingress/client/pds_ingress_client.py
@@ -53,9 +53,19 @@ SUMMARY_TABLE = {
 
 
 def fatal_code(err: requests.exceptions.RequestException) -> bool:
-    """Only retry for common transient errors"""
+    """
+    Determines if the HTTP return code associated with a requests exception
+    corresponds to a fatal error or not. If the error is of a transient nature,
+    this function will return False, indicating to the backoff decorator that
+    the reqeust should be retried. Otherwise, a return value of True will
+    cause any backoff/reties to be abandoned.
+    """
     if err.response is not None:
-        return 400 <= err.response.status_code < 500
+        # HTTP codes indicating a transient error (including throttling) which
+        # are worth retrying after a backoff
+        transient_codes = [408, 425, 429, 500, 502, 503, 504, 509]
+
+        return err.response.status_code not in transient_codes
     else:
         # No response to interrogate, so default to no retry
         return True
