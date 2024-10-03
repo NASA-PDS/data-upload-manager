0d71a0b8f1ba9b008443a96a3ca52d385af6499d
Scott Collins <collinss@jpl.nasa.gov>
2024-10-03 11:15:29 -0500

    Updated Terraform scripts for the lambda ingress service to support instantiation of the bucket-map.yaml based on venue

--- terraform/modules/lambda/service/variables.tf
+++ terraform/modules/lambda/service/variables.tf
@@ -1,4 +1,9 @@
 
+variable "venue" {
+    type        = string
+    description = "Name of the venue to be deployed to. Should be one of [dev,test,prod]"
+}
+
 variable "lambda_s3_bucket_name" {
   type        = string
   description = "Name of the S3 bucket to upload Lambda artifacts to"
@@ -19,4 +24,54 @@ variable "lambda_ingress_service_function_description" {
 variable "lambda_ingress_service_iam_role_arn" {
   type        = string
   description = "IAM role ARN to allocate to the Ingress Service Lambda function"
+}
+
+variable "lambda_ingress_service_default_buckets" {
+  description = "List of the default S3 staging buckets to create for each PDS Node, each name will be appended with designated venue name to form the final bucket name"
+  type = list(
+    object(
+      {
+        name        = string
+        description = string
+      }
+    )
+  )
+  default = [
+    {
+      name        = "pds-atm-staging",
+      description = "Default staging bucket for PDS Atmospheres Node"
+    },
+    {
+      name        = "pds-eng-staging"
+      description = "Default staging bucket for PDS Engineering Node"
+    },
+    {
+      name        = "pds-geo-staging"
+      description = "Default staging bucket for PDS Geosciences Node"
+    },
+    {
+      name        = "pds-img-staging"
+      description = "Default staging bucket for PDS Cartography and Imaging Sciences Discipline Node"
+    },
+    {
+      name        = "pds-naif-staging"
+      description = "Default staging bucket for PDS Navigational and Ancillary Information Facility Node"
+    },
+    {
+      name        = "pds-ppi-staging"
+      description = "Default staging bucket for PDS Planetary Plasma Interactions Node"
+    },
+    {
+      name        = "pds-rs-staging"
+      description = "Default staging bucket for PDS Radio Science Node"
+    },
+    {
+      name        = "pds-rms-staging",
+      description = "Default staging bucket for PDS Ring-Moon Systems Node"
+    },
+    {
+      name        = "pds-sbn-staging",
+      description = "Default staging bucket for PDS Small Bodies Node"
+    }
+  ]
 }
